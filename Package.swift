c22028519321a9a0604582b52da404777c795a93
Thiago Holanda <unnamedd@gmail.com>
2018-01-13 02:56:28 +0100

    added products and targets on Package.swift

--- Package.swift
+++ Package.swift
@@ -4,16 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "tips",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    name: "Tips",
+    products: [
+        .executable(
+            name: "tips",
+            targets: ["Tips"]
+        ),
+        .library(
+            name: "TipsCore",
+            targets: ["TipsCore"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "tips",
-            dependencies: []),
+            name: "Tips",
+            dependencies: ["TipsCore"]
+        ),
+        .target(name: "TipsCore"),
+        .testTarget(
+            name: "TipsTests",
+            dependencies: ["TipsCore"]
+        ),
     ]
 )
